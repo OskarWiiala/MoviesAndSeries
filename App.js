fc0cae313bf60b6cbba105f7e47a2466dfe6bf39
OskarWiiala <pullumastery@gmail.com>
2021-02-19 13:23:59 +0200

    ready to start working

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Project application Initialized. I have already made most files, so let's get working!</Text>
       <StatusBar style="auto" />
     </View>
   );
